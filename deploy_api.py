04ee7ced1453388a105acee9e5b2e489cf7c4b0a
卿爽 <541400574@qq.com>
2024-09-28 10:36:05 +0800

    更新代码

--- deploy_api.py
+++ deploy_api.py
@@ -15,6 +15,9 @@ from database.response import CRUD
 from bson.json_util import dumps
 from datetime import datetime
 from pymongo import ReturnDocument
+from settings import WXURL, APPID, SECRET
+import requests
+
 
 app = FastAPI()
 creator = IDCreator()
@@ -146,48 +149,7 @@ async def photo_add_background(
     return result_messgae
 
 
-# 透明图像添加纯色背景接口 传递图像是base64格式
-@app.post("/v1/add_background")
-async def v1_photo_add_background(
-    input_image_base64: str = Form(...),
-    color: str = Form(...),
-    kb: str = Form(None),
-    render: int = Form(0),
-):
-    render_choice = ["pure_color", "updown_gradient", "center_gradient"]
-    image_bytes = base64.b64decode(input_image_base64)
-    nparr = np.frombuffer(image_bytes, np.uint8)
-    img = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
 
-    color = hex_to_rgb(color)
-    color = (color[2], color[1], color[0])
-
-    result_image = add_background(
-        img,
-        bgr=color,
-        mode=render_choice[render],
-    ).astype(np.uint8)
-
-    if kb:
-        result_image = cv2.cvtColor(result_image, cv2.COLOR_RGB2BGR)
-        result_image_base64 = resize_image_to_kb_base64(result_image, int(kb))
-    else:
-        result_image_base64 = numpy_2_base64(result_image)
-
-    # try:
-    result_messgae = {
-        "status": True,
-        "image_base64": result_image_base64,
-    }
-
-    # except Exception as e:
-    #     print(e)
-    #     result_messgae = {
-    #         "status": False,
-    #         "error": e
-    #     }
-
-    return result_messgae
 # 六寸排版照生成接口
 @app.post("/generate_layout_photos")
 async def generate_layout_photos(
@@ -231,6 +193,54 @@ async def generate_layout_photos(
 
     return result_messgae
 
+# <!----------------------------------自己写的接口------自己写的接口-----------自己写的接口-------自己写的接口------------------------------->
+
+
+# 透明图像添加纯色背景接口 传递图像是base64格式
+@app.post("/v1/add_background")
+async def v1_photo_add_background(
+    input_image_base64: str = Form(...),
+    color: str = Form(...),
+    kb: str = Form(None),
+    render: int = Form(0),
+):
+    render_choice = ["pure_color", "updown_gradient", "center_gradient"]
+    image_bytes = base64.b64decode(input_image_base64)
+    nparr = np.frombuffer(image_bytes, np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_UNCHANGED)
+
+    color = hex_to_rgb(color)
+    color = (color[2], color[1], color[0])
+
+    result_image = add_background(
+        img,
+        bgr=color,
+        mode=render_choice[render],
+    ).astype(np.uint8)
+
+    if kb:
+        result_image = cv2.cvtColor(result_image, cv2.COLOR_RGB2BGR)
+        result_image_base64 = resize_image_to_kb_base64(result_image, int(kb))
+    else:
+        result_image_base64 = numpy_2_base64(result_image)
+
+    # try:
+    result_messgae = {
+        "status": True,
+        "image_base64": result_image_base64,
+    }
+
+    # except Exception as e:
+    #     print(e)
+    #     result_messgae = {
+    #         "status": False,
+    #         "error": e
+    #     }
+
+    return result_messgae
+
+
+
 @app.get("/photo_size")
 async def test(name: str = Query(default=None, title='证件照名称'),category: int = Query(default=None, title='证件照类型')
                ,recommend: int = Query(default=None, title='是否热门')):
@@ -263,14 +273,16 @@ async def test(name: str = Query(default=None, title='证件照名称'),category
 
 @app.get("/login")
 async def test(code: str = Query(default=None, title='微信code')):
-    # 当前时间
-    current_time = datetime.utcnow()
+    url=WXURL+'?appid='+APPID+'&secret='+SECRET+'&js_code='+code+'&grant_type=authorization_code'
 
+    response = requests.get(url)
+    response_json= response.json()
+    openid= response_json['openid']
     # 尝试查找是否有相应的 code 并更新登录时间与登录次数
     result =  collection_user.find_one_and_update(
-        {"code": code},  # 查询条件
+        {"openid": openid},  # 查询条件
         {
-            "$set": {"last_login": current_time},  # 更新最后登录时间
+            "$set": {"last_login": datetime.utcnow()},  # 更新最后登录时间
             "$inc": {"login_count": 1}  # 登录次数加 1
         },
         upsert=False,  # 不自动插入
@@ -283,9 +295,9 @@ async def test(code: str = Query(default=None, title='微信code')):
     else:
         # 如果没有找到对应的文档，插入一条新的记录
         new_document = {
-            "code": code,
-            "created_at": current_time,
-            "last_login": current_time,
+            "openid": openid,
+            "created_at": datetime.utcnow(),
+            "last_login": datetime.utcnow(),
             "login_count": 1
         }
         insert_result =  collection_user.insert_one(new_document)
@@ -301,10 +313,6 @@ async def test():
         "image_base64": "123",
     }
 
-    # except Exception as e:
-    #     result_messgae = {
-    #         "status": False,
-    #     }
 
     return result_messgae
 
@@ -316,13 +324,8 @@ async def test():
         "status": "123",
         "image_base64": "123",
     }
-
-    # except Exception as e:
-    #     result_messgae = {
-    #         "status": False,
-    #     }
-
     return result_messgae
+
 
 if __name__ == "__main__":
     import uvicorn
